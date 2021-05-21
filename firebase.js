00e07d62cebe2958282e1a2ffe754846a1dd6604
daimessdn@gmail.com <daimessdn@gmail.com>
2021-05-22 00:47:57 +0700

    set up React Navigation

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyATL427GHdD0fv1IVZq8sbdv8tao1y0Ics",
+  authDomain: "my-signal-clone.firebaseapp.com",
+  projectId: "my-signal-clone",
+  storageBucket: "my-signal-clone.appspot.com",
+  messagingSenderId: "621503344194",
+  appId: "1:621503344194:web:8603a429054115fc6807db"
+};
