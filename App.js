83b9e37b22e4172f37e595d2c805c59d6ac57409
daimessdn@gmail.com <daimessdn@gmail.com>
2021-05-22 08:55:45 +0700

    added Login screen

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// importing React Navigation packages
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// importing external components
+import LoginScreen from "./screens/LoginScreen";
+
+// initial stack
+const Stack = createStackNavigator();
+
 export default function App() {
+  // set up global screen options style
+  const globalScreenOptions = {
+    headerStyle: { backgroundColor: "#2c6bed" },
+    headerTitleStyle: { color: "white" },
+    headerTintColor: "white"
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
