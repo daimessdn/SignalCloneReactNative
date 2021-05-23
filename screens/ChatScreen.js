ca7d8a82717eced72d63c81848756fd790e7c2a0
daimessdn@gmail.com <daimessdn@gmail.com>
2021-05-23 19:00:20 +0700

    resolved chat style for receiver and sender

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -19,10 +19,13 @@ import { StatusBar } from "expo-status-bar";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 
-import { db } from "../firebase";
+import * as firebase from "firebase";
+
+import { auth, db } from "../firebase";
 
 export default function AddChatScreen({ navigation, route }) {
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -74,14 +77,34 @@ export default function AddChatScreen({ navigation, route }) {
   const sendMessage = () => {
     Keyboard.dismiss();
 
-    db.collection("chats").doc(route.params.id).collect("messages").add({
+    db.collection("chats").doc(route.params.id).collection("messages").add({
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       message: input,
       displayName: auth.currentUser.displayName,
       email: auth.currentUser.email,
       photoURL: auth.currentUser.photoURL,
     });
+
+    setinput("");
   };
+
+  useLayoutEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .doc(route.params.id)
+      .collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+
+    return unsubscribe;
+  }, [route]);
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
@@ -94,7 +117,50 @@ export default function AddChatScreen({ navigation, route }) {
       >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <>
-            <ScrollView>{/* chat goes here */}</ScrollView>
+            <ScrollView contentContainerStyle={{ paddingTop: 15 }}>
+              {/* chat goes here */}
+
+              {messages.map(({ id, data }) =>
+                data.email === auth.currentUser.email ? (
+                  <View key={id} style={styles.receiver}>
+                    <Avatar
+                      containerStyle={{
+                        position: "absolute",
+                        bottom: -15,
+                        right: -5,
+                      }}
+                      position="absolute"
+                      rounded
+                      bottom={-15}
+                      right={-5}
+                      size={30}
+                      source={{ uri: data.photoURL }}
+                    />
+
+                    <Text style={styles.receiverText}>{data.message}</Text>
+                  </View>
+                ) : (
+                  <View key={id} style={styles.sender}>
+                    <Avatar
+                      containerStyle={{
+                        position: "absolute",
+                        bottom: -15,
+                        left: -5,
+                      }}
+                      position="absolute"
+                      rounded
+                      bottom={-15}
+                      left={-5}
+                      size={30}
+                      source={{ uri: data.photoURL }}
+                    />
+
+                    <Text style={styles.senderText}>{data.message}</Text>
+                    <Text style={styles.senderName}>{data.displayName}</Text>
+                  </View>
+                )
+              )}
+            </ScrollView>
 
             <View style={styles.footer}>
               <TextInput
@@ -118,6 +184,43 @@ export default function AddChatScreen({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  receiver: {
+    padding: 15,
+    backgroundColor: "#ececec",
+    alignSelf: "flex-end",
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: "80%",
+    position: "relative",
+  },
+  sender: {
+    padding: 15,
+    backgroundColor: "#ececec",
+    alignSelf: "flex-start",
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: "80%",
+    position: "relative",
+  },
+  senderText: {
+    left: 10,
+    fontWeight: "500",
+    marginLeft: 10,
+    marginBottom: 15,
+  },
+  receiverText: {
+    fontWeight: "500",
+    marginLeft: 10,
+    color: "black",
+  },
+  senderName: {
+    left: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: "white",
   },
   footer: {
     flexDirection: "row",
