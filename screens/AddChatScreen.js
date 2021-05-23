ad89164480f3dae94b9caca2144af11031a2b88c
daimessdn@gmail.com <daimessdn@gmail.com>
2021-05-23 11:27:28 +0700

    resolved HomeScreen header and added AddChatScreen

---
+++ screens/AddChatScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+
+export default function AddChatScreen() {
+  return (
+    <View><Text>This is a add chat screen</Text></View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,11 +3,20 @@ import { StyleSheet, View, KeyboardAvoidingView, ScrollView, TouchableOpacity } 
 import { StatusBar } from "expo-status-bar";
 import { Button, Input, Text, Avatar } from "react-native-elements";
 
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
+
 import CustomListItem from "../components/CustomListItem";
 
 import { auth } from "../firebase";
 
 export default function HomeScreen({ navigation }) {
+  // sign out user function
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Signal",
@@ -16,8 +25,24 @@ export default function HomeScreen({ navigation }) {
       headerTintColor: "black",
       headerLeft: () => (
         <View style={{ marginLeft: 20 }}>
-          <TouchableOpacity>
+          <TouchableOpacity activeOpacity={.5} onPress={signOutUser}>
             <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerRight: () => (
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: 80,
+          marginRight: 20
+        }}>
+          <TouchableOpacity activeOpacity={.5}>
+            <AntDesign name="camerao" size={24} color="black" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => navigation.navigate("AddChat")} activeOpacity={.5}>
+            <SimpleLineIcons name="pencil" size={24} color="black" />
           </TouchableOpacity>
         </View>
       )
