1596809a90406b2db73cbc8e042ec50efb3812a4
daimessdn@gmail.com <daimessdn@gmail.com>
2021-05-23 14:00:22 +0700

    added function to add chat

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -3,6 +3,8 @@ import { StyleSheet, View, Text } from "react-native";
 import { Button, Input } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
+import { db } from "../firebase";
+
 export default function AddChatScreen({ navigation }) {
   // init'd empty input state
   const [input, setInput] = useState("");
@@ -14,20 +16,32 @@ export default function AddChatScreen({ navigation }) {
     });
   }, [navigation]);
 
+  // function to create chat
+  const createChat = async () => {
+    await db.collection("chats").add({ chatName: input }).then(() => { navigation.goBack(); }).catch((error) => alert(error));
+  }
+
   return (
-    <View styles={styles.container}>
+    <View style={styles.container}>
       <Input
         placeholder="Enter a chat name"
         value={input}
         onChangeText={(value) => setInput(value)}
+        onSubmitEditing={createChat}
         leftIcon={
-          <Icon name="wechat" type="antdesign" size={24} color="black" />
+          <Icon name="comments" type="antdesign" size={24} color="black" />
         }
       />
+
+      <Button onPress={createChat} title="Create a new chat" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "white",
+    padding: 30,
+    height: "100%"
+  },
 });
